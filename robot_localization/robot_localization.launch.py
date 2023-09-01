503f761b592e9a17c0f369b48214e91a527f0282
Max Polzin <polzin.max@gmail.com>
2023-09-01 14:18:02 +0200

    Update.

--- robot_localization/robot_localization.launch.py
+++ robot_localization/robot_localization.launch.py
@@ -13,15 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
-import launch_ros.actions
-import os
-import yaml
-from launch.substitutions import EnvironmentVariable
-import pathlib
-import launch.actions
-from launch.actions import DeclareLaunchArgument
 
 from launch_ros.actions import Node
 
